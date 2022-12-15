ca1ec9243c74ded8973fa0a1d8a7f72662bc87e0
PhonicUK <phonicuk@googlemail.com>
2022-12-15 11:42:19 +0000

    Update ampstart.sh

--- ampstart.sh
+++ ampstart.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+echo "[Info] AMPStart for Docker - v22.08.1"
+
 if [ -z "${AMPUSERID}" ]; then
-  echo "This docker image cannot be used directly by itself - it must be started by ampinstmgr"
+  echo "[Info] This docker image cannot be used directly by itself - it must be started by ampinstmgr"
   exit 100
 fi
 
@@ -9,14 +11,18 @@ fi
 getent passwd amp &> /dev/null
 
 if [ "$?" == "0" ]; then
-    echo "AMP user already exists, continuing..."
+    echo "[Info] AMP user already exists, continuing..."
 else
-    useradd -m -d /home/amp -s /bin/bash -c "AMP Process User" -u $AMPUSERID amp &> /dev/null
+    echo "[Info] Performing first-time container setup..."
+    useradd -m -d /home/amp -s /bin/bash -c "AMP Process User" -u $AMPUSERID -g $AMPGROUPID amp &> /dev/null
     touch /home/amp/.gitconfig
     chown -R amp:amp /home/amp 2> /dev/null
     usermod -aG tty amp
+    chmod +x /AMP/AMP_Linux_x86_64
+    echo "[Info] Container setup complete."
 fi
 
 ARGS=$@
-exec su -l -m -c "cd /AMP; HOME=/home/amp /AMP/AMP_Linux_x86_64 ${ARGS}; exit $?" -- amp
+exec su -l -c "ampinstmgr --sync-certs" -- amp
+exec su -l -c "cd /AMP; HOME=/home/amp /AMP/AMP_Linux_x86_64 ${ARGS}; exit $?" -- amp
 exit $?
