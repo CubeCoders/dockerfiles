3a84b23d71c3938d8d9e5ac1fdd2cd3d022572fb
PhonicUK <phonicuk@googlemail.com>
2023-03-22 11:45:47 +0000

    Add files via upload

--- ampstart.sh
+++ ampstart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "[Info] AMPStart for Docker - v22.08.1"
+echo "[Info] AMPStart for Docker - v22.12.1"
 
 if [ -z "${AMPUSERID}" ]; then
   echo "[Info] This docker image cannot be used directly by itself - it must be started by ampinstmgr"
@@ -14,7 +14,8 @@ if [ "$?" == "0" ]; then
     echo "[Info] AMP user already exists, continuing..."
 else
     echo "[Info] Performing first-time container setup..."
-    useradd -m -d /home/amp -s /bin/bash -c "AMP Process User" -u $AMPUSERID -g $AMPGROUPID amp &> /dev/null
+    groupadd -r -g $AMPGROUPID amp > /dev/null
+    useradd -m -d /home/amp -s /bin/bash -c "AMP Process User" -u $AMPUSERID -g $AMPGROUPID amp > /dev/null
     touch /home/amp/.gitconfig
     chown -R amp:amp /home/amp 2> /dev/null
     usermod -aG tty amp
@@ -23,6 +24,5 @@ else
 fi
 
 ARGS=$@
-exec su -l -c "ampinstmgr --sync-certs" -- amp
-exec su -l -c "cd /AMP; HOME=/home/amp /AMP/AMP_Linux_x86_64 ${ARGS}; exit $?" -- amp
+exec su -l -c "ampinstmgr --sync-certs; cd /AMP; HOME=/home/amp /AMP/AMP_Linux_x86_64 ${ARGS}; exit $?" -- amp
 exit $?
--- updateDockerImages.sh
+++ updateDockerImages.sh
@@ -3,6 +3,9 @@ mypath=`realpath $0`
 cd `dirname $mypath`
 
 docker pull $(cat AMPDockerFile | grep FROM | cut -f 2 -d ' ')
-./buildDockerBase.sh
+./buildDockerBase
+yes | docker container prune
+yes | docker image prune -a
+yes | docker volume prune
 exit
 
