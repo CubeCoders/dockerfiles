f3a64ed548bea8406ed72ad6fecda564c94c15f6
Greelan <53196309+Greelan@users.noreply.github.com>
2025-09-11 11:49:57 +1000

    fix(wine): fix previous

--- scripts/wine/find-deps.sh
+++ scripts/wine/find-deps.sh
@@ -23,7 +23,8 @@ STATUS=/tmp/dpkg.status.empty
 
 case "${BUILD}" in
   stable|devel|staging)
-    apt-get -s install -y -o Dir::State::status="$STATUS" -o APT::Install-Recommends=1 -o APT::Architectures=amd64;i386 "winehq-${BUILD}" \
+    apt-get -s install -y -o Dir::State::status="$STATUS" -o APT::Install-Recommends=1 -o APT::Architectures=amd64 -o APT::Architectures=i386 \
+      "winehq-${BUILD}" \
     | _print_inst
     ;;
 
@@ -44,7 +45,7 @@ case "${BUILD}" in
       exit 2
     fi
 
-    apt-get -s install -y -o Dir::State::status="$STATUS" -o APT::Install-Recommends=1 -o APT::Architectures=amd64;i386 \
+    apt-get -s install -y -o Dir::State::status="$STATUS" -o APT::Install-Recommends=1 -o APT::Architectures=amd64 -o APT::Architectures=i386 \
       "wine-${WINE_BRANCH}-i386=${WINE_BUILD}" \
       "wine-${WINE_BRANCH}-amd64=${WINE_BUILD}" \
       "wine-${WINE_BRANCH}=${WINE_BUILD}" \
